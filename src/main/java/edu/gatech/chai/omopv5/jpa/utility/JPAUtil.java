562fe07f2ad40417b2a15522288402496af50491
Myung Choi <myungchoi@gmail.com>
2020-12-16 02:34:00 -0500

    OMOP version added

--- src/main/java/edu/gatech/chai/omopv5/jpa/utility/JPAUtil.java
+++ src/main/java/edu/gatech/chai/omopv5/jpa/utility/JPAUtil.java
@@ -21,18 +21,20 @@ import javax.persistence.Persistence;
 
 public class JPAUtil {
 	private static final String PERSISTENCE_UNIT_NAME = "GT-FHIR2";
-	  private static EntityManagerFactory factory;
+	private static EntityManagerFactory factory;
+	
+	public static final String omopVersion = "5.2";
 
-	  public static EntityManagerFactory getEntityManagerFactory() {
-	    if (factory == null) {
-	      factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-	    }
-	    return factory;
-	  }
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		return factory;
+	}
 
-	  public static void shutdown() {
-	    if (factory != null) {
-	      factory.close();
-	    }
-	  }
+	public static void shutdown() {
+		if (factory != null) {
+			factory.close();
+		}
+	}
 }
